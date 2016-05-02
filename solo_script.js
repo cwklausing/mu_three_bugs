f70872f44ac1003ecdc79df83acdf5f21678166f
cwklausing <cwklausing@gmail.com>
2016-05-02 14:57:40 -0500

    project debugged

--- solo_script.js
+++ solo_script.js
@@ -9,14 +9,14 @@ var arrayScout = ["Scout", "6243", "74750", 5];
 var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
 
 //Create variables used to write to the DOM
-var newEl, newText, position;
+var newEl, newText;
 //Capture the position of insertion into the DOM
-position = document.getElementById('content');
+var position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
 for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array);
+	array[i] = calculateSTI(array[i]);
  	newEl = document.createElement('li');
 	newText = document.createTextNode(array[i]);
 	newEl.appendChild(newText);
@@ -33,13 +33,13 @@ function calculateSTI(array){
   var reviewScore = array[3];
 
   var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+
   if(bonus > 0.13){
     bonus = 0.13;
   }
-
   newArray[1] = bonus;
-  newArray[2] = baseSalary * (1.0 + bonus);
-  newArray[3] = baseSalary * bonus;
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
+  newArray[3] = Math.round(baseSalary * bonus);
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
   return newArray;
 }
@@ -63,7 +63,7 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent - 1;
+  return basePercent;
 }
 
 function getYearAdjustment(employeeNumber){
@@ -77,6 +77,7 @@ function getYearAdjustment(employeeNumber){
 function getIncomeAdjustment(salary){
   var incomeAdjustment = 0;
   salary = parseInt(salary);
+
   if(salary > 65000){
     incomeAdjustment = 0.01;
   }
